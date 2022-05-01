b82131c644bb13cd30b814c7b6c2472b7591c23b
Sergio <Sergioamaya538@gmail.com>
2022-05-01 16:30:34 -0700

    completed display order and get_order

--- async-point-of-sale-system-template/main.py
+++ async-point-of-sale-system-template/main.py
@@ -1,4 +1,5 @@
 import asyncio
+from re import sub
 from inventory import Inventory
 from order import Order
 
@@ -38,9 +39,87 @@ def display_catalogue(catalogue):
 
     print("\n------------------------------\n")
 
+async def get_order(inventory, num_items):
+    order = Order(inventory)
+    tasks = []
+
+    print("Please enter the number of items that you would like to add to your order. Enter q to quit")    
+
+    while True:
+        item_id = input("Enter an item number: ")
+        if item_id == "q":
+            break
+
+        if not item_id.isdigit():
+            print("Please enter a valid number.")
+            continue
+        
+        item_id = int(item_id)
+        if item_id > num_items:
+            print(f"Please enter a number below {num_items +1}")
+            continue
+        add_to_order_task = asyncio.create_task(order.add_item(item_id))
+        tasks.append(add_to_order_task)
+
+    print("Placing order")
+
+    for task in tasks:
+        in_stock, item_id = await task
+
+        if not in_stock:
+            print(f"Unfortunately item number {item_id} is out of stock and has been removed from your order")
+    
+    return order
+
+def display_order(order, sub_total, tax, total):
+    print("\n Here is a summary of your order: \n")
+    print(order)
+    print(f"\nSubtotal: ${round(sub_total, 2)}")
+    print(f"\nTax: ${round(tax, 2)}")
+    print(f"\nTotal: ${round(total, 2)}")
+    
+
+
+def purchase_order():
+    pass    
+
+
+
+
 
 async def main():
-    pass
+    print("Welcome to the ProgrammingExpert Burger Bar!")
+
+    inventory = Inventory()
+
+    num_items_task = asyncio.create_task(inventory.get_number_of_items)
+
+    print("Loading the catalogue")
+    catalogue = await inventory.get_catalogue()
+    display_catalogue(catalogue)
+
+    num_items = await num_items_task
+
+    while True:
+        order = await get_order(inventory, num_items)
+
+        sub_total = order.get_price()
+        if sub_total != 0:
+            tax = sub_total *.05
+            total = sub_total +tax
+
+            display_order(order, sub_total, tax, total)
+            purchase_order(total)
+
+        order_again = input("Would you like to make another order? (yes or no)?").lower()
+
+        if order_again not in ["y", "yes"]:
+            break
+
+        print()
+
+    print("Goodbye!")    
+
 
 if __name__ == "__main__":
     asyncio.run(main())
